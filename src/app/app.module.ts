0529302d482d76be98c6e6499a9ec1e49137b17f
m.naghsh <m.naghsh@CLN18598>
2022-01-13 10:40:38 +0330

    Add route.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSliderModule } from '@angular/material/slider';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+ // { path: 'ali', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    MatSliderModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    [RouterModule.forRoot(routes)],
   ],
   providers: [],
   bootstrap: [AppComponent]
